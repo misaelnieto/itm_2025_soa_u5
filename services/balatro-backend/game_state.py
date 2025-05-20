c01e7668c42f9b6b16a642f46fe7a7e24df9d749
Karim Soto <karimtec2003@gmail.com>
2025-05-19 18:05:00 -0700

    Estado inicial del backend

---
+++ services/balatro-backend/game_state.py
@@ -0,0 +1,15 @@
+from services.deck_handler import PlayerDeckHandler
+
+
+class GameState():
+    def __init__(self):
+        self.p1_sid = None
+        self.p2_sid = None
+        self.p1_score = 0
+        self.p2_score = 0
+        self.p1_last_score = 0
+        self.p2_last_score = 0
+        self.current_player = 1
+        self.current_turn = 0
+        self.p1_hand_handler = PlayerDeckHandler()
+        self.p2_hand_handler = PlayerDeckHandler()
